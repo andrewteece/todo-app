d029bd5ce050ba55c4b604e37270ba1ce604b5e2
andrewteece <andrewteece@gmail.com>
2025-07-04 09:52:05 -0500

    convert px to rem

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import './index.css';
+import BackgroundHeading from './components/BackgroundHeading';
 
 function App() {
   return (
-    <>
-      <div className='min-h-screen bg-red-500 text-white flex items-center justify-center'>
-        <h1 className='font-bold underline text-blue-600 text-3xl'>
-          Hello Tailwind!
-        </h1>
-      </div>
-    </>
+    <div className='font-sans bg-[hsl(32,69%,82%)] min-h-screen flex justify-center items-center flex-col'>
+      <BackgroundHeading />
+
+      <main className='relative w-[60.75rem] shadow-[0_4px_4px_rgb(0,0,0,0.08)] h-[39.75rem] bg-[#fff] rounded-[.5rem] overflow-hidden grid grid-cols-[7fr_4fr] grid-rows-[3.6875rem_1fr]'></main>
+    </div>
   );
 }
 
---
+++ src/components/BackgroundHeading.tsx
@@ -0,0 +1,10 @@
+export default function BackgroundHeading() {
+  return (
+    <h1
+      className='text-[7.5rem] font-bold text-[rgba(0,0,0,0.05)] uppercase absolute left-2/4 -translate-x-1/2 
+    tracking-[0.2em]'
+    >
+      TodoApp
+    </h1>
+  );
+}
