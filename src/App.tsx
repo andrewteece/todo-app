530d709c4eba84c0f677fd4ba88ea4c2b03e8771
andrewteece <andrewteece@gmail.com>
2025-07-04 10:41:25 -0500

    Footer component and import into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import BackgroundHeading from './components/BackgroundHeading';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='font-sans bg-[hsl(32,69%,82%)] min-h-screen flex justify-center items-center flex-col'>
       <BackgroundHeading />
 
-      <main className='relative w-[60.75rem] shadow-[0_4px_4px_rgb(0,0,0,0.08)] h-[39.75rem] bg-[#fff] rounded-[.5rem] overflow-hidden grid grid-cols-[7fr_4fr] grid-rows-[3.6875rem_1fr]'></main>
+      <main className='relative w-[60.75rem] shadow-[0_4px_4px_rgb(0,0,0,0.08)] h-[39.75rem] bg-[#fff] rounded-[.5rem] overflow-hidden grid grid-cols-[7fr_4fr] grid-rows-[3.6875rem_1fr]'>
+        <header className='col-[1/3] row-[1/2] bg-[hsl(34,64%,96%)] border-b border-black/[0.08]'></header>
+        <ul></ul>
+
+        <section className='col-[2/3] row-[2/3] bg-[hsl(30,100%,99%)] border-l border-black/[0.08]'></section>
+      </main>
+      <Footer />
     </div>
   );
 }
