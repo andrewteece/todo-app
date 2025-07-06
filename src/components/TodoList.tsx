a2fab08467b34507929c3785d31e554a980e5206
andrewteece <andrewteece@gmail.com>
2025-07-06 10:59:48 -0500

    update Button and TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,18 @@
+const initialTodos = ['buy groceries', 'feed the cat', 'call plumber'];
+
 export default function TodoList() {
-  return <ul></ul>;
+  return (
+    <ul>
+      {initialTodos.map((todo) => (
+        <li
+          key={todo}
+          className='flex justify-between items-center px-8 h-[3.125rem] text-[1rem] cursor-pointer
+          border-b border-black/[0.08]'
+        >
+          <span>{todo}</span>
+          <button>❌</button>
+        </li>
+      ))}
+    </ul>
+  );
 }
