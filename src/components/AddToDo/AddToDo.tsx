5601b24fbb9fcba1b8ddbdd6aba85cfe141d71aa
andrewteece <andrewteece@gmail.com>
2025-07-19 09:24:31 -0500

    remove module.css

--- src/components/AddToDo/AddToDo.tsx
+++ src/components/AddToDo/AddToDo.tsx
@@ -1,11 +1,1 @@
-import styles from './AddToDo.module.css';
 
-<div className={styles.rowAddTask}>
-  <input
-    type='text'
-    value={inputValue}
-    onChange={handleInputChange}
-    placeholder='Enter a new task'
-  />
-  <button onClick={handleAddTask}>Add Task</button>
-</div>;
